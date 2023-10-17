e9c2c2c8982a182dac109640cecb6ab7fadcf066
Bruno Rodrigo Vieira <brunovieira439@gmail.com>
2023-10-16 23:14:22 -0300

    fixing incorrect arguments for render postgres-db

--- src/main/resources/db/migration/V1__initial-sql.sql
+++ src/main/resources/db/migration/V1__initial-sql.sql
@@ -1,6 +1,3 @@
 SET client_encoding = 'UTF8';
-CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
-COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
 SET default_tablespace = '';
-SET default_with_oids = false;
 SET timezone TO 'UTC';
